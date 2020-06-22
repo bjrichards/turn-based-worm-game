9a86c00aa8d3e32cac4448d1b42bfa9f46249e16
bjrichards <braedenrichards@nevada.unr.edu>
2020-06-21 21:31:54 -0700

    new tile types: new wood type and new highlighting

---
+++ base_ui_map_cell_values.js
@@ -0,0 +1,98 @@
+baseUI = [
+    {
+        "id" : "000",
+        "entireRow" : "1",
+        "rowNumber" : "0",
+        "tile" : "wood_0"
+    },
+    {
+        "id" : "001",
+        "entireRow" : "1",
+        "rowNumber" : "1",
+        "tile" : "wood_0"
+    },
+    {
+        "id" : "002",
+        "entireRow" : "1",
+        "rowNumber" : "2",
+        "tile" : "wood_0"
+    },
+    {
+        "id" : "003",
+        "entireRow" : "1",
+        "rowNumber" : "3",
+        "tile" : "wood_0"
+    },
+    {
+        "id" : "004",
+        "entireRow" : "1",
+        "rowNumber" : "4",
+        "tile" : "wood_0"
+    },
+    {
+        "id" : "005",
+        "entireRow" : "1",
+        "rowNumber" : "5",
+        "tile" : "wood_0"
+    },
+    {
+        "id" : "006",
+        "entireRow" : "1",
+        "rowNumber" : "6",
+        "tile" : "wood_0"
+    },
+    {
+        "id" : "007",
+        "entireRow" : "1",
+        "rowNumber" : "7",
+        "tile" : "wood_0"
+    },
+    {
+        "id" : "008",
+        "entireRow" : "1",
+        "rowNumber" : "8",
+        "tile" : "wood_0"
+    },
+    {
+        "id" : "009",
+        "entireRow" : "1",
+        "rowNumber" : "9",
+        "tile" : "wood_0"
+    },
+    {
+        "id" : "010",
+        "entireRow" : "1",
+        "rowNumber" : "10",
+        "tile" : "wood_0"
+    },
+    {
+        "id" : "011",
+        "entireRow" : "1",
+        "rowNumber" : "11",
+        "tile" : "wood_0"
+    },
+    {
+        "id" : "012",
+        "entireRow" : "1",
+        "rowNumber" : "12",
+        "tile" : "wood_0"
+    },
+    {
+        "id" : "013",
+        "entireRow" : "1",
+        "rowNumber" : "13",
+        "tile" : "wood_0"
+    },
+    {
+        "id" : "014",
+        "entireRow" : "1",
+        "rowNumber" : "14",
+        "tile" : "wood_0"
+    },
+    {
+        "id" : "015",
+        "entireRow" : "1",
+        "rowNumber" : "15",
+        "tile" : "wood_0"
+    }
+];
--- image_mapping.js
+++ image_mapping.js
@@ -13,9 +13,21 @@
             "overwritable" : "0"
         },
         {
-            "id" : "00",
+            "id" : "02",
             "name" : "grass_0",
             "image" : "img/grass_tile_0.png",
+            "overwritable" : "0"
+        },
+        {
+            "id" : "03",
+            "name" : "highlight_0",
+            "image" : "img/highlight.png",
+            "overwritable" : "0"
+        },
+        {
+            "id" : "04",
+            "name" : "wood_0",
+            "image" : "img/wood_tile_0.png",
             "overwritable" : "0"
         }
     ];
