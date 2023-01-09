7058defc11c98791900ab42ce80d61eeda7d31a7
bjrichards <braedenrichards@nevada.unr.edu>
2023-01-09 09:45:15 -0800

    Fixed issue stemming from missing images

--- image_mapping.js
+++ image_mapping.js
@@ -1,51 +1,33 @@
 
-    let Tiles = [
-        { 
-            "id" : "00",
-            "name" : "background_0",
-            "image" : "img/background_tile_0.png",
-            "overwritable" : "1"
-        },
-        { 
-            "id" : "01",
-            "name" : "ground_0",
-            "image" : "img/ground_tile_0.png",
-            "overwritable" : "0"
-        },
-        {
-            "id" : "02",
-            "name" : "grass_0",
-            "image" : "img/grass_tile_0.png",
-            "overwritable" : "0"
-        },
-        {
-            "id" : "03",
-            "name" : "highlight_0",
-            "image" : "img/highlight.png",
-            "overwritable" : "0"
-        },
-        {
-            "id" : "04",
-            "name" : "wood_0",
-            "image" : "img/wood_tile_0.png",
-            "overwritable" : "0"
-        },
-        {
-            "id" : "04",
-            "name" : "wood_1",
-            "image" : "img/wood_1.png",
-            "overwritable" : "0"
-        },
-        {
-            "id" : "05",
-            "name" : "metal_0",
-            "image" : "img/metal_0.png",
-            "overwritable" : "0"
-        },
-        {
-            "id" : "05",
-            "name" : "metal_1",
-            "image" : "img/metal_1.png",
-            "overwritable" : "0"
-        }
-    ];
+let Tiles = [
+    {
+        "id": "00",
+        "name": "background_0",
+        "image": "img/background_tile_0.png",
+        "overwritable": "1"
+    },
+    {
+        "id": "01",
+        "name": "ground_0",
+        "image": "img/ground_tile_0.png",
+        "overwritable": "0"
+    },
+    {
+        "id": "02",
+        "name": "grass_0",
+        "image": "img/grass_tile_0.png",
+        "overwritable": "0"
+    },
+    {
+        "id": "03",
+        "name": "highlight_0",
+        "image": "img/highlight.png",
+        "overwritable": "0"
+    },
+    {
+        "id": "04",
+        "name": "wood_0",
+        "image": "img/wood_tile_0.png",
+        "overwritable": "0"
+    }
+];
